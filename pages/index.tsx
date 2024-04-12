f1bc9f796f0772c1d442e3023d15dafb5ff73e74
diego <diegoeliseoiovane@gmail.com>
2024-04-12 15:29:11 -0300

    clenup

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ export default function HomePage() {
         <meta name="viewport" content="width=device-width, initial-scale=1" key="Compured" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>Prueba</div>
+      <a href="rawbt:Print,%20Test%0A%0A"> Print Test </a>
     </>
   )
 }
