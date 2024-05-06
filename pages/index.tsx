d46bf2c67e045a06dd56f5607bb4a6ca5f804a73
diego <diegoeliseoiovane@gmail.com>
2024-05-06 15:45:05 -0300

    test layout for oscars

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,12 +15,31 @@ export default function HomePage() {
         <meta name="viewport" content="width=device-width, initial-scale=1" key="Compured" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="w-full pt-5 flex gap-2 justify-center">
-        <a className="bg-zinc-100 rounded-md p-4" href="rawbt:Print%20Test1%0A%0A"> Print Test </a>
-        <button className="bg-zinc-100 rounded-md p-4" onClick={handleClick}> Print Test 2</button>
-      </div>
-      <div className='print-area'>
-        Print test for kitchen only
+      <div className="flex flex-col h-screen bg-zinc-100">
+        <div className="bg-white h-16 grid place-items-center">
+          <h1>Categories</h1>
+        </div>
+        <div className="flex-1 grid grid-cols-12 gap-2 px-2 pb-24 pt-2 overflow-auto">
+          <div className="col-span-6 bg-zinc-300 rounded-md aspect-square"></div>
+          <div className="col-span-6 bg-zinc-300 rounded-md aspect-square"></div>
+          <div className="col-span-6 bg-zinc-300 rounded-md aspect-square"></div>
+          <div className="col-span-6 bg-zinc-300 rounded-md aspect-square"></div>
+          <div className="col-span-6 bg-zinc-300 rounded-md aspect-square"></div>
+          <div className="col-span-6 bg-zinc-300 rounded-md aspect-square"></div>
+          <div className="col-span-6 bg-zinc-300 rounded-md aspect-square"></div>
+          <div className="col-span-6 bg-zinc-300 rounded-md aspect-square"></div>
+          <div className="col-span-6 bg-zinc-300 rounded-md aspect-square"></div>
+          <div className="col-span-6 bg-zinc-300 rounded-md aspect-square"></div>
+          <div className="col-span-6 bg-zinc-300 rounded-md aspect-square"></div>
+          <div className="col-span-6 bg-zinc-300 rounded-md aspect-square"></div>
+          <div className="col-span-6 bg-zinc-300 rounded-md aspect-square"></div>
+          <div className="col-span-6 bg-zinc-300 rounded-md aspect-square"></div>
+          <div className="col-span-6 bg-zinc-300 rounded-md aspect-square"></div>
+          <div className="col-span-6 bg-zinc-300 rounded-md aspect-square"></div>
+        </div>
+        <div className="fixed bottom-0 z-50 bg-white h-20 w-full">
+          <h2>Cart</h2>
+        </div>
       </div>
     </>
   )
